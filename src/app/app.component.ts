8d3e0ad180aa194763e754c27f4fa0441f951a95
Maxime <maxime.b2494@gmail.com>
2024-09-18 15:34:47 +0200

    utilisation de l'api signal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Hello, World !';
+
+  name = signal('Pikachu');
+  life = signal(21);
+  
+  size = computed(() => {
+    if (this.life() <= 15) {
+      return 'Petit';
+    }
+
+    if (this.life() >= 25) {
+      return 'Grand';
+    }
+
+    return 'Moyen';
+  });
+
+  // counter = signal(0);
+  // doubleCounter = computed(() => this.counter() * 2)
+  // tripleCounter = computed(() => this.counter() * 3)
+
+  // constructor() {
+  //   effect(() => {
+  //     console.log('Le compteur a été mis à jour :', this.counter());
+  //     console.log('Le compteur double a été mis à jour :', this.doubleCounter());
+  //     console.log('Le compteur triple a été mis à jour :', this.tripleCounter());
+  //   })
+  // }
+
+  incrementLife(){
+    this.life.update(n => n + 1);
+  }
+
+  decrementLife(){
+    this.life.update(n => n - 1);
+  }
+
+
+  // increment(){
+  //   this.counter.update(n => n + 1);
+  // }
+
+  // reset(){
+  //   this.counter.set(0);
+  //   console.clear()
+  // }
 }
